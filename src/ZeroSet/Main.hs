a954d928bcc2450dc57b0b577fbe35715453bd6c
Frank Staals <frank@fstaals.net>
2020-04-21 14:05:33 +0200

    support for disks and polies

--- src/ZeroSet/Main.hs
+++ src/ZeroSet/Main.hs
@@ -1,22 +1,26 @@
 module ZeroSet.Main where
 
+import Data.Geometry.Properties
 import Control.Lens
 import Data.Data
 import Data.Ext
+import Data.Geometry.Ball
 import Data.Geometry.Box
+import Data.Geometry.Ellipse (ellipseToCircle)
 import Data.Geometry.Ipe
 import Data.Geometry.PolyLine
 import Data.Geometry.ZeroSet
+import Data.Maybe (mapMaybe)
+import Data.RealNumber.Rational
+import Data.Util
 import Options.Applicative
--- import Data.RealNumber.Rational
 
-import Debug.Trace
+-- import Debug.Trace
 --------------------------------------------------------------------------------
 
 
--- type R = RealNumber 10
-
-type R = Double
+type R = RealNumber 10
+-- type R = Double
 
 data Options = Options { inFile  :: FilePath
                        , outFile :: FilePath
@@ -42,25 +46,45 @@ mainWith (Options inFile outFile) = readSinglePageFile inFile >>= \case
     Right p  -> runPage p outFile
 
 
-data ReadInput = ReadInput (PolyLine 2 () R)
-                           (PolyLine 2 () R)
-                           (Rectangle () R)
+
+
+
+data Geoms  = Polies (Two (PolyLine 2 () R))
+            | Disks  (Two (Disk () R))
+            deriving (Show,Eq)
+
+data ReadInput = ReadInput Geoms (Rectangle () R)
                deriving (Show,Eq)
 
 
 -- type
 
-readInput page | traceShow page False = undefined
-readInput page = (\(p1,p2) r -> ReadInput p1 p2 r) <$> polies <*> rect
+-- readInput page | traceShow page False = undefined
+readInput      :: IpePage R -> Either String ReadInput
+readInput page = (\ps r -> ReadInput ps r) <$> geoms <*> rect
   where
     rect   = case page^..content.traverse._IpePath.core._asRectangle of
                [rect'] -> Right rect'
                _       -> Left "no rectangle"
-    polies = case page^..content.traverse._IpePath.core._asPolyLine of
-               [p1,p2] -> Right (p1,p2)
-               pls     -> Left $ "no 2 polylines " <> show pls
+
+    geoms = (Polies <$> readPolylines page) <|> (Disks <$> readDisks page)
 
 
+readAll' :: (HasDefaultFromIpe g, r ~ NumType g)
+         => IpePage r -> [g :+ IpeAttributes (DefaultFromIpe g) r]
+readAll' = readAll . Identity
+
+readPolylines      :: IpePage R -> Either String (Two (PolyLine 2 () R))
+readPolylines page = case map (^.core) $ readAll' page of
+                       [p1,p2] -> Right $ Two p1 p2
+                       pls     -> Left $ "no 2 polylines " <> show pls
+
+readDisks      :: IpePage R -> Either String (Two (Disk () R))
+readDisks page =  case mapMaybe (ellipseToDisk) . map (^.core) $ readAll' page of
+                      [c1,c2] -> Right $ Two c1 c2
+                      pls     -> Left $ "no 2 disks " <> show pls
+  where
+    ellipseToDisk = fmap (view $ from _DiskCircle) . ellipseToCircle
 
 -- newtype IOE e a = IOE { runIOE :: IO (Either e a) }
 
@@ -78,8 +102,10 @@ runPage page fp = case res of
                     Left e   -> print e
                     Right pl -> writeIpeFile fp $ singlePageFromContent [iO $ defIO pl]
   where
-    res = do (ReadInput p1 p2 rect) <- readInput page
-             withError "tracing failed" $ traceFromPrefix defaultZeroConfig p1 p2 rect
+    res = do (ReadInput gs rect) <- readInput page
+             withError "tracing failed" $ case gs of
+               Polies (Two p1 p2) -> traceFromPrefix    defaultZeroConfig p1 p2 rect
+               Disks  (Two d1 d2) -> traceBisectorDisks defaultZeroConfig d1 d2 rect
 
 
 withError   :: e -> Maybe a -> Either e a
